a3fb7b90d2a0926f3e9e0b28a32419f29e05f20c
Vladimir Linkov <rackotmell@gmail.com>
2026-07-31 18:33:49 +0300

    Rewrite Future/Promise without mutex

--- include/future.h
+++ include/future.h
@@ -1,196 +1,195 @@
 #pragma once
 
 #include <atomic>
-#include <condition_variable>
 #include <cstdint>
 #include <exception>
 #include <memory>
-#include <mutex>
 #include <optional>
+#include <type_traits>
 #include <variant>
+
+#include "logger.h"
 
 namespace miniruntime {
 
+    enum class FutureStatus {
+        Pending,
+        Ready,
+        Closed
+    };
+
+    struct FutureStateBase {
+        std::atomic<FutureStatus> status{FutureStatus::Pending};
+        std::atomic<uint64_t> id;
+        std::atomic<bool> idSetted{false};
+        std::atomic<bool> blocked{false};
+    };
+
     template<typename T>
-    struct FutureState {
-        std::mutex mutex;
-        std::condition_variable cv;
+    struct FutureState : FutureStateBase {
         std::variant<std::monostate, T, std::exception_ptr> result;
-        std::atomic<bool> ready{false};
-        std::optional<uint64_t> id;
-        std::atomic<bool> closed{false};
     };
 
     template<>
-    struct FutureState<void> {
-        std::mutex mutex;
-        std::condition_variable cv;
-        std::optional<std::exception_ptr> result;
-        std::atomic<bool> ready{false};
-        std::optional<uint64_t> id;
-        std::atomic<bool> closed{false};
+    struct FutureState<void> : FutureStateBase {
+        std::variant<std::monostate, std::exception_ptr> result;
     };
+
+    // Explicit declare concept
+    // FutureState must have result field with std::variant type 
+    template<typename T>
+    struct isVariant : std::false_type {};
+
+    template<typename... Args>
+    struct isVariant<std::variant<Args...>> : std::true_type {};
+
+    template<typename T>
+    concept Variant = isVariant<std::remove_cvref_t<T>>::value;
+
+    template<typename T>
+    concept HasVariantResult = requires (T& obj) {
+        obj.result;
+    } && Variant<decltype(std::declval<T>().result)>;
 
 
     template<typename T>
     class Promise;
 
-    template<typename T>
-    class Future {
+    template<HasVariantResult State>
+    class FutureBase {
     public:
-        Future(Future&&) = default;
-        Future& operator=(Future&&) = default; 
+        FutureBase(FutureBase&&) = default;
+        FutureBase& operator=(FutureBase&&) = default;
+
+        bool isReady() {
+            return m_state->status.load(std::memory_order_acquire) == FutureStatus::Ready;
+        }
+
+        std::optional<uint64_t> getId() {
+            if (m_state->idSetted.load(std::memory_order_acquire)) {
+                return m_state->id.load(std::memory_order_acquire);
+            }
+            return std::nullopt;
+        }
+
+        bool isClosed() {
+            return m_state->status.load(std::memory_order_acquire) == FutureStatus::Closed;
+        }
+
+    protected:
+        explicit FutureBase(std::shared_ptr<State> state)
+            : m_state(state) {}
+
+        std::shared_ptr<State> m_state;
+    };
+
+    template<typename T>
+    class Future : public FutureBase<FutureState<T>> {
+        friend class Promise<T>;
+    public:
+        using FutureBase<FutureState<T>>::FutureBase;
 
         std::optional<T> get() {
-            std::unique_lock<std::mutex> lock(m_state->mutex);
-            m_state->cv.wait(lock, [this]{
-                return m_state->ready || m_state->closed;
-            });
+            this->m_state->status.wait(FutureStatus::Pending, std::memory_order_acquire);
 
-            if (m_state->closed)
+            if (this->m_state->status.load(std::memory_order_acquire) == FutureStatus::Closed)
                 return std::nullopt;
             
-            auto& result = m_state->result;
+            auto& result = this->m_state->result;
             if (std::holds_alternative<std::exception_ptr>(result))
                 std::rethrow_exception(std::get<std::exception_ptr>(result));
 
             return std::get<T>(result);
         }
-
-        bool isReady() {
-            return m_state->ready.load();
-        }
-
-        std::optional<uint64_t> getId() {
-            std::lock_guard<std::mutex> lock(m_state->mutex);
-            return m_state->id;
-        }
-
-        bool isClosed() {
-            return m_state->closed.load();
-        }
-        
-    private:
-        friend class Promise<T>;
-        explicit Future(std::shared_ptr<FutureState<T>> state)
-            : m_state(state) {}
-
-        std::shared_ptr<FutureState<T>> m_state;
     };
 
     template<>
-    class Future<void> {
+    class Future<void> : public FutureBase<FutureState<void>> {
+        friend class Promise<void>;
     public:
-        Future(Future&&) = default;
-        Future& operator=(Future&&) = default; 
+        using FutureBase<FutureState<void>>::FutureBase;
 
         void get() {
-            std::unique_lock<std::mutex> lock(m_state->mutex);
-            m_state->cv.wait(lock, [this]{
-                return m_state->ready || m_state->closed;
-            });
+            this->m_state->status.wait(FutureStatus::Pending, std::memory_order_acquire);
 
-            if (m_state->closed)
+            if (this->m_state->status.load(std::memory_order_acquire) == FutureStatus::Closed)
                 return;
             
-            auto& result = m_state->result;
-            if (result)
-                std::rethrow_exception(*result);
+            auto& result = this->m_state->result;
+            if (std::holds_alternative<std::exception_ptr>(result))
+                std::rethrow_exception(std::get<std::exception_ptr>(result));
         }
-
-        bool isReady() {
-            return m_state->ready.load();
-        }
-
-        std::optional<uint64_t> getId() {
-            std::lock_guard<std::mutex> lock(m_state->mutex);
-            return m_state->id;
-        }
-
-        bool isClosed() {
-            return m_state->closed.load();
-        }
-
-    private:
-        friend class Promise<void>;
-        explicit Future(std::shared_ptr<FutureState<void>> state)
-            : m_state(state) {}
-
-        std::shared_ptr<FutureState<void>> m_state;
     };
 
 
-    template<typename T>
-    class Promise {
+    template<HasVariantResult State>
+    class PromiseBase {
     public:
-        Promise() : m_state(std::make_shared<FutureState<T>>()) {}
-
-        void setValue(T value) {
-            std::lock_guard<std::mutex> lock(m_state->mutex);
-            m_state->result.template emplace<T>(std::move(value));
-            m_state->ready.store(true);
-            m_state->cv.notify_all();
-        }
+        PromiseBase() : m_state(std::make_shared<State>()) {}
 
         void setException(std::exception_ptr ptr) {
-            std::lock_guard<std::mutex> lock(m_state->mutex);
+            if (!claim(m_state->blocked)) {
+                LOG_WARNING("Promise exception allready setted up");
+                return;
+            }
             m_state->result.template emplace<std::exception_ptr>(std::move(ptr));
-            m_state->ready.store(true);
-            m_state->cv.notify_all();
+            m_state->status.store(FutureStatus::Ready, std::memory_order_release);
+            m_state->status.notify_all();
+        }
+
+        void setId(uint64_t id) {
+            claim(m_state->idSetted);
+            m_state->id.store(id, std::memory_order_release);
+        }
+
+        void close() {
+            m_state->status.store(FutureStatus::Closed, std::memory_order_release);
+            m_state->status.notify_all();
+        }
+        
+    protected:
+        bool claim(std::atomic<bool>& target) {
+            bool expected = false;
+            return target.compare_exchange_strong(expected, true,
+                std::memory_order_acq_rel, std::memory_order_acquire);
+        }
+
+        std::shared_ptr<State> m_state;
+    };
+
+    template<typename T>
+    class Promise : public PromiseBase<FutureState<T>> {
+    public:
+        void setValue(T value) {
+            if (!this->claim(this->m_state->blocked)) {
+                    LOG_WARNING("Promise<T> value already setted up");
+                    return;
+            }
+            this->m_state->result.template emplace<T>(std::move(value));
+            this->m_state->status.store(FutureStatus::Ready, std::memory_order_release);
+            this->m_state->status.notify_all();
         }
 
         Future<T> getFuture() {
-            return Future<T>(m_state);
+            return Future<T>(this->m_state);
         }
-
-        void setId(uint64_t id) {
-            std::lock_guard<std::mutex> lock(m_state->mutex);
-            m_state->id = id;
-        }
-
-        void close() {
-            m_state->closed.store(true);
-            m_state->cv.notify_all();
-        }
-
-    private:
-        std::shared_ptr<FutureState<T>> m_state;
     };
 
     template<>
-    class Promise<void> {
+    class Promise<void> : public PromiseBase<FutureState<void>> {
     public:
-        Promise() : m_state(std::make_shared<FutureState<void>>()) {}
-
         void setValue() {
-            std::lock_guard<std::mutex> lock(m_state->mutex);
-            m_state->ready.store(true);
-            m_state->cv.notify_all();
-        }
-
-        void setException(std::exception_ptr ptr) {
-            std::lock_guard<std::mutex> lock(m_state->mutex);
-            m_state->result = std::move(ptr);
-            m_state->ready.store(true);
-            m_state->cv.notify_all();
+            if (!this->claim(this->m_state->blocked)) {
+                    LOG_WARNING("Promise<void> value already setted up");
+                    return;
+            }
+            this->m_state->status.store(FutureStatus::Ready, std::memory_order_release);
+            this->m_state->status.notify_all();
         }
 
         Future<void> getFuture() {
-            return Future<void>(m_state);
+            return Future<void>(this->m_state);
         }
-
-        void setId(uint64_t id) {
-            std::lock_guard<std::mutex> lock(m_state->mutex);
-            m_state->id = id;
-        }
-
-        void close() {
-            m_state->closed.store(true);
-            m_state->cv.notify_all();
-        }
-
-    private:
-        std::shared_ptr<FutureState<void>> m_state;
     };
 
 }
